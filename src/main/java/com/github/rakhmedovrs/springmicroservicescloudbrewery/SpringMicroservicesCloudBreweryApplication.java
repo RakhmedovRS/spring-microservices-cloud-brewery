0b16dc34b3168d57ffb1229012217a9643ab0741
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2021-04-23 11:21:56 -0700

    Initial commit

---
+++ src/main/java/com/github/rakhmedovrs/springmicroservicescloudbrewery/SpringMicroservicesCloudBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.github.rakhmedovrs.springmicroservicescloudbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroservicesCloudBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroservicesCloudBreweryApplication.class, args);
+	}
+
+}
